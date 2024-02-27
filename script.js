77091a316ccac7cd9dc4c6cad7df2c891f0dbe08
Joel Kwihangana <joelkwijoe@gmail.com>
2024-02-27 23:25:50 +0200

    fetching data from api using javaScript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+// get quotes from the API
+
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("No quote generated", error);
+  }
+}
+
+//onload
+getQuote();
